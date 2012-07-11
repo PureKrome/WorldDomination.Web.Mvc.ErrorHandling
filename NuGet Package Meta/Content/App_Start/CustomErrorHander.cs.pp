0bc21f912b6e11c6a45b4d1e2e4ec7302bdb6531
Pure Krome <github@world-domination.com.au>
2012-07-11 12:40:29 +1000

    - Updated NuGet package to v 2.1.2. - NuGet package now adds the CustomErrorHandler during the intall :)

---
+++ NuGet Package Meta/Content/App_Start/CustomErrorHander.cs.pp
@@ -0,0 +1,16 @@
+﻿using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using WorldDomination.Web.Mvc;
+using $rootnamespace$.App_Start;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(CustomErrorHander), "PreStart")]
+namespace $rootnamespace$.App_Start
+{
+    public static class CustomErrorHander
+    {
+        public static void PreStart()
+        {
+            // Register the custom error handling module.
+            DynamicModuleUtility.RegisterModule(typeof (CustomErrorHandlingModule));
+        }
+    }
+}
